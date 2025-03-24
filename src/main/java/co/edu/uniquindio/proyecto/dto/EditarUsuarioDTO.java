708d068486d36f77a2d2be5bf76c8be0a05f5b19
YarleyMejia <100544029+YarleyMejia@users.noreply.github.com>
2025-03-24 11:36:06 -0500

    Se suben la categoria de carpetas y clases principales del proyecto

---
+++ src/main/java/co/edu/uniquindio/proyecto/dto/EditarUsuarioDTO.java
@@ -0,0 +1,15 @@
+package co.edu.uniquindio.proyecto.dto;
+
+import co.edu.uniquindio.proyecto.modelo.enums.Ciudad;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+public record EditarUsuarioDTO(
+        @NotBlank String id,
+        @NotBlank @Length(max = 100) String nombre,
+        @NotNull Ciudad ciudad,
+        @NotBlank @Length(max = 100) String direccion,
+        @Length(max = 10) String telefono
+) {
+}
---
+++ src/main/java/co/edu/uniquindio/proyecto/excepciones/RestExceptionHandler.java
@@ -0,0 +1,49 @@
+package co.edu.uniquindio.proyecto.excepciones;
+
+import co.edu.uniquindio.proyecto.dto.MensajeDTO;
+import co.edu.uniquindio.proyecto.dto.ValidacionDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<MensajeDTO<String>> noResourceFoundExceptionHandler(NoResourceFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body( new MensajeDTO<>(true, "El recurso no fue encontrado") );
+    }
+
+    @ExceptionHandler(ElementoNoEncontradoException.class)
+    public ResponseEntity<MensajeDTO<String>> elementoNoEncontradoExceptionHandler(ElementoNoEncontradoException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body( new MensajeDTO<>(true, ex.getMessage()) );
+    }
+
+    @ExceptionHandler(ElementoRepetidoException.class)
+    public ResponseEntity<MensajeDTO<String>> elementoRepetidoExceptionHandler(ElementoRepetidoException ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body( new MensajeDTO<>(true, ex.getMessage()) );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<MensajeDTO<String>> generalExceptionHandler(Exception e){
+        return ResponseEntity.internalServerError().body( new MensajeDTO<>(true, e.getMessage()) );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<MensajeDTO<List<ValidacionDTO>>> validationExceptionHandler(MethodArgumentNotValidException ex ) {
+
+        List<ValidacionDTO> errores = ex.getFieldErrors()
+                .stream()
+                .map(x -> new ValidacionDTO(x.getField(), x.getDefaultMessage()))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().body( new MensajeDTO<>(true, errores) );
+    }
+
+}
